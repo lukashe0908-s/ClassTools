a76700be2a366f67cedc8425d0ab855170a0b822
Lukas <52617815+LukasHe0908@users.noreply.github.com>
2024-07-01 13:14:18 +0800

    Update build.js

--- main/build/nextron/build.js
+++ main/build/nextron/build.js
@@ -38,8 +38,9 @@ const execaOptions = {
     await Promise.all([fs.remove(appDir), fs.remove(distDir)]);
 
     logger.info('Building renderer process');
-    await execa('next', ['build', path.join(cwd, rendererSrcDir)], execaOptions);
-    await Promise.all([fs.moveSync(path.join(rendererSrcDir, '../build/out'), appDir)]);
+    //await execa('next', ['build', path.join(cwd, rendererSrcDir)], execaOptions);
+    //await Promise.all([fs.moveSync(path.join(rendererSrcDir, '../build/out'), appDir)]);
+    await Promise.all([fs.copySync(path.join(rendererSrcDir, 'public/static'), path.join(appDir, 'static'))]);
 
     logger.info('Building main process');
     await execa('node', [path.join(__dirname, './configs/webpack.config.production.js')], execaOptions);
