85f3c189fb3aefd034ee2ad5f3a8b08985b30a84
LukasHe0908 <lukashe908@gmail.com>
2024-08-10 23:25:26 +0800

    update

--- renderer/app/settings/privacy/page.tsx
+++ renderer/app/settings/privacy/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Card, CardBody, Switch, Button } from '@nextui-org/react';
+import { Card, CardBody, Switch, Button, CardHeader } from '@nextui-org/react';
 import axios from 'axios';
 import { getConfigSync } from '../../../components/p_function';
 export default function App() {
@@ -10,6 +10,8 @@ export default function App() {
           <CardBody className='block'>
             <p>你正在发送必需诊断数据</p>
             <span className='text-gray-600 text-sm'>帮助改进软件并使其保特安全、最新并按预期工作</span>
+            <br /> <br />
+            <span>备份：域名2024/7到期，目前为HTTP传输</span>
           </CardBody>
         </Card>
         <div className='flex gap-4 flex-wrap'>
@@ -18,58 +20,32 @@ export default function App() {
           </Switch>
           <Switch>配置云端备份</Switch>
         </div>
-        <Button
-          onClick={() => {
-            (async () => {
-              let foo = await axios.post('https://s.lukass.link/pastebin/api.php/edit/backup_dt', JSON.stringify(await getConfigSync()));
-              let bar = await axios.post(
-                'https://s.lukass.link/pastebin_with_oss/process.php?path=/setFile',
-                JSON.stringify({
-                  password: '5de4c32b-595d-47b4-a924-b0f456e72c8a',
-                  folder: 'f1798983-42d0-4e49-809c-944c8742817f/',
-                  name: 'backup_dt.txt',
-                  content: JSON.stringify(await getConfigSync()),
-                }),
-                {
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                }
-              );
-              console.log(foo.data, bar.data);
-              alert(foo.data + '\n' + JSON.stringify(bar.data));
-              alert();
-            })();
-          }}
-        >
-          备份
-        </Button>
-        <Button
-          onClick={() => {
-            (async () => {
-              let foo = await axios.post('https://s.lukass.link/pastebin/api.php/edit/backup_dt2', JSON.stringify(await getConfigSync()));
-              let bar = await axios.post(
-                'https://s.lukass.link/pastebin_with_oss/process.php?path=/setFile',
-                JSON.stringify({
-                  password: '5de4c32b-595d-47b4-a924-b0f456e72c8a',
-                  folder: 'f1798983-42d0-4e49-809c-944c8742817f/',
-                  name: 'backup_dt2.txt',
-                  content: JSON.stringify(await getConfigSync()),
-                }),
-                {
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                }
-              );
-              console.log(foo.data, bar.data);
-              alert(foo.data + '\n' + JSON.stringify(bar.data));
-              alert();
-            })();
-          }}
-        >
-          备份2
-        </Button>
+        <div className='flex flex-wrap gap-1'>
+          <Button
+            onClick={() => {
+              (async () => {
+                let foo = await axios.post('https://us.lukas1.eu.org/pastebin/api.php/edit/backup_dt', JSON.stringify(await getConfigSync()));
+                console.log(foo.data);
+                alert(foo.data);
+                alert();
+              })();
+            }}
+          >
+            备份
+          </Button>
+          <Button
+            onClick={() => {
+              (async () => {
+                let foo = await axios.post('https://us.lukas1.eu.org/pastebin/api.php/edit/backup_dt2', JSON.stringify(await getConfigSync()));
+                console.log(foo.data);
+                alert(foo.data);
+                alert();
+              })();
+            }}
+          >
+            备份2
+          </Button>
+        </div>
       </div>
     </>
   );
