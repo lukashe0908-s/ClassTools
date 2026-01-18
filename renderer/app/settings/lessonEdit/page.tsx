44b96a6369f268701f3c0481f00966b4dc8a1cae
LukasHe0908 <52617815+LukasHe0908@users.noreply.github.com>
2026-01-18 10:25:34 +0800

    fix: remove mui lab

--- renderer/app/settings/lessonEdit/page.tsx
+++ renderer/app/settings/lessonEdit/page.tsx
@@ -1,9 +1,3 @@
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
 export default function App() {
   return (
     <>
@@ -12,56 +6,6 @@ export default function App() {
           渲染序列
         </span>
       </div>
-      <span className='ml-4'>近期课程管理可能重构为类似 Classland 的编辑方式</span>
-      <Timeline
-        sx={{
-          [`& .${timelineItemClasses.root}:before`]: {
-            flex: 0,
-            padding: 0,
-          },
-        }}>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot color='primary' />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>初始值</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>列表（名称）</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>列表（时间）</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>替换（天）</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>替换（课程）</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot color='success' />
-          </TimelineSeparator>
-          <TimelineContent>完成</TimelineContent>
-        </TimelineItem>
-      </Timeline>
     </>
   );
 }
