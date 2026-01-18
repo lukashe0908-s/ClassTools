89c81e7800a58ac5d81df6cd41ec066807648cd0
LukasHe0908 <52617815+LukasHe0908@users.noreply.github.com>
2026-01-18 16:31:29 +0800

    fix: update NextConfig,remove monaco-editor

--- renderer/next.config.ts
+++ renderer/next.config.ts
@@ -1,22 +1,6 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
-  trailingSlash: true,
   distDir: process.env.NODE_ENV === 'production' ? '../build/app' : '.next',
-  // outputFileTracing: false,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(
-        new MonacoWebpackPlugin({
-          languages: ['javascript', 'typescript'],
-          filename: 'static/[name].worker.js',
-        })
-      );
-    }
-    // Important: return the modified config
-    return config;
-  },
   turbopack: {},
 };
