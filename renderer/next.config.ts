1c1c2fda43581ddb1e3da8f72649e5e928f57bab
LukasHe0908 <52617815+LukasHe0908@users.noreply.github.com>
2026-01-18 16:36:45 +0800

    fix: can't load locally

--- renderer/next.config.ts
+++ renderer/next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
+  trailingSlash: true,
   distDir: process.env.NODE_ENV === 'production' ? '../build/app' : '.next',
   turbopack: {},
 };
